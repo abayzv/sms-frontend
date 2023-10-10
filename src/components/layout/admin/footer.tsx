31722e1329dcb1c41b428681e9f30fadfb70af77
DESKTOP-9VU9I7S\Rai <ajipunk008@gmail.com>
2023-10-10 19:40:46 +0700

    delete unecesary component

--- src/components/layout/admin/footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer(){
-    return (
-        <div className="py-10 mb-8 mt-auto text-neutral-400">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, nisi!
-        </div>
-    )
-}
